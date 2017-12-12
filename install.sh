b73dc59b09df98effd3e956d8046a673a5949a87
Michael Hearne <mhearne@usgs.gov>
2017-12-12 08:24:02 -0700

    Fixed comparison operator in install script

--- install.sh
+++ install.sh
@@ -36,10 +36,12 @@ fi
 # If the user has specified the -r (reset) flag, then create an
 # environment based on only the named dependencies, without
 # any versions of packages specified.
-if [ $reset eq 1 ]; then
+if [ $reset -eq 1 ]; then
     echo "Ignoring platform, letting conda sort out dependencies..."
     env_file=environment_basic.yml
 fi
+
+echo "Using ${env_file}"
 
 # Turn off whatever other virtual environment user might be in
 source deactivate
