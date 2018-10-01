a41076808320fbfde9fc234ce2bc2d4b11331878
Michael Hearne <mhearne@usgs.gov>
2018-10-01 09:15:57 -0600

    Now using grids for regional distances, as this does not lead to weird projection related errors.

--- strec/gmreg.py
+++ strec/gmreg.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
 
 # stdlib imports
-import glob
 import os.path
 from collections import OrderedDict
-from functools import partial
 
 # third party
-import fiona
-from shapely.geometry import shape as tShape
-from shapely.geometry.point import Point
-from shapely.geometry.linestring import LineString
-from shapely.geometry.multipolygon import MultiPolygon
-from shapely.ops import nearest_points
-from shapely.ops import transform
-import pyproj
 import numpy as np
-from obspy.geodetics.base import gps2dist_azimuth
 import pandas as pd
+from mapio.geodict import GeoDict
+from mapio.reader import read
 
 # local imports
 from strec.utils import get_config
 
 # As we add more layers, define their name/file mappings here.
 # All of these files have polygons where the attribute is true.
-REGIONS = OrderedDict()
-REGIONS['Stable'] = 'stable.geojson'
-REGIONS['Active'] = 'active.geojson'
-REGIONS['Subduction'] = 'subduction.geojson'
-REGIONS['Volcanic'] = 'volcanic.geojson'
+TECTONIC_REGIONS = {1: 'DistanceToStable',
+                    2: 'DistanceToActive',
+                    3: 'DistanceToVolcanic',
+                    4: 'DistanceToSubduction'}
+OCEANIC_REGIONS = {1: 'DistanceToOceanic',
+                   0: 'DistanceToContinental'}
+
+DX = DY = 0.0083333333
+XSPAN = YSPAN = 4.0
 
 # for each of the above regions, when we're inside a polygon, we should
 # capture the field below as the "Tectonic Domain".
@@ -50,78 +45,55 @@ GEOGRAPHIC = 'geographic.json'
 GEOGRAPHIC_FIELD = 'REG_NAME'
 
 
-def _get_nearest_point(point, shape):
-    """Return distance from point to nearest vertex of a polygon.
-
-    Args:
-        point (Point): Shapely point object (lon,lat).
-        shape (shape): Shapely geometry (usually Polygon).
-
-    Returns:
-        float: Minimum distance in km from input point to nearest vertex on input shape.
-    """
-    latlong = pyproj.Proj(
-        '+proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees')
-    azim = pyproj.Proj('+proj=aeqd +lat_0=%.6f +lon_0=%.6f' %
-                       (point.y, point.x))
-
-    project = partial(
-        pyproj.transform,
-        latlong,
-        azim)
-
-    ppoint = transform(project, point)
-    pshape = transform(project, shape)
-
-    if isinstance(pshape, MultiPolygon):
-        shapelist = pshape
-    else:
-        shapelist = [pshape]
-
-    mindist = 99999999999999999999
-    for pshape in shapelist:
-        pshape_string = LineString(pshape.exterior)
-        point_nearest, shape_nearest = nearest_points(ppoint, pshape)
-        distance = np.sqrt(shape_nearest.x**2 + shape_nearest.y**2) / 1000
-        if distance < mindist:
-            mindist = distance
-
-    return mindist
+def geodetic_distance(lon1, lat1, lon2, lat2):
+    distance = 6371.0 * \
+        np.sqrt(((lon1 - lon2) * np.cos(0.5 * (lat1 + lat2)))
+                ** 2 + (lat1 - lat2)**2)
+    return distance
 
 
-def _get_layer_info(layer, point, fname):
-    """Get information about a given layer with respect to an input point.
+def get_dist_to_type(center_lon, center_lat, grid, regions):
+    gd = grid.getGeoDict()
+    #
+    # The distance calculation wants everything in radians
+    #
+    lons = np.radians(np.linspace(gd.xmin, gd.xmax, gd.nx))
+    lats = np.radians(np.linspace(gd.ymin, gd.ymax, gd.ny))
+    mlons, mlats = np.meshgrid(lons, lats)
 
-    Args:
-        layer (str): Name of input layer ("Stable","Active",etc.)
-        point (Point): Shapely Point object (lon,lat).
-        fname (str): Path to file containing spatial data for layer.
-    Returns:
-        str or None: None if point is not inside layer, name of region if it is.
-        float: Distance from point to nearest point on layer.
-        bool: Boolean indicating whether layer has a back-arc subduction region.
-    """
-    distance_to_layer = 99999999999999999
-    shapes = fiona.open(fname, 'r')
-    inside = False
-    domain = None
-    region = None
-    for shape in shapes:
-        pshape = tShape(shape['geometry'])
-        if pshape.contains(point):
-            inside = True
-            break
-        else:
-            dist_to_vertex = _get_nearest_point(point, pshape)
-            if dist_to_vertex < distance_to_layer:
-                distance_to_layer = dist_to_vertex
-    if inside:
-        region = layer
-        distance = _get_nearest_point(point, pshape)
-    else:
-        distance = distance_to_layer
+    center_lon_rad = np.radians(center_lon)
+    center_lat_rad = np.radians(center_lat)
 
-    return (region, distance)
+    #
+    # The type at the epicenter is the type in the middle of the grid
+    # (I'm not sure about the "- 1" part)
+    #
+    midx = int(gd.nx / 2 - 1)
+    midy = int(gd.ny / 2 - 1)
+    mytype = grid._data[midy, midx]
+    #
+    # Tectonic types are 1: stable, 2: active, 3: volcanic, 4: subduction
+    # so iniitalize the zero element of the array NaN and the others Inf
+    #
+    distances = np.ones(len(regions))*np.inf
+    dist_to_type = dict(zip(list(regions.values()), distances))
+    tectype = regions[mytype]
+    dist_to_type[tectype] = 0
+    for tec_code in regions.keys():
+        tectype = regions[tec_code]
+        if tec_code == mytype:
+            continue
+        ixx = grid._data == tec_code
+        if not np.any(ixx):
+            continue
+        tlons = mlons[ixx].flatten()
+        tlats = mlats[ixx].flatten()
+        dists = geodetic_distance(center_lon_rad,
+                                  center_lat_rad,
+                                  tlons, tlats)
+
+        dist_to_type[tectype] = np.min(dists)
+    return dist_to_type
 
 
 class Regionalizer(object):
@@ -129,19 +101,12 @@ class Regionalizer(object):
         """Determine tectonic region information given epicenter and depth.
 
         Args:
-            datafolder (str): Path to directory containing spatial data for 
-                tectonic regions.
+            datafolder (str): Path to directory containing spatial data
+            for tectonic regions.
         """
         self._datafolder = datafolder
-        self._regions = OrderedDict()
-        for layer, fname in REGIONS.items():
-            fullfile = os.path.join(datafolder, fname)
-            if not os.path.isfile(fullfile):
-                raise OSError('File %s not found' % fullfile)
-            self._regions[layer] = fullfile
-        self._oceanic = os.path.join(datafolder, OCEANIC)
-        if not os.path.isfile(self._oceanic):
-            raise OSError('File %s not found' % self._oceanic)
+        self._tectonic_grid = os.path.join(datafolder, 'tectonic_global.grd')
+        self._oceanic_grid = os.path.join(datafolder, 'oceanic_global.grd')
 
     @classmethod
     def load(cls):
@@ -166,43 +131,47 @@ class Regionalizer(object):
                 - TectonicRegion: Subduction, Active, Stable, or Volcanic.
                 - DistanceToStable: Distance in km to nearest stable region.
                 - DistanceToActive: Distance in km to nearest active region.
-                - DistanceToSubduction: Distance in km to nearest subduction region.
-                - DistanceToVolcanic: Distance in km to nearest volcanic region.
+                - DistanceToSubduction: Distance in km to nearest subduction
+                                        region.
+                - DistanceToVolcanic: Distance in km to nearest volcanic
+                                      region.
                 - Oceanic: Boolean indicating if epicenter is in the ocean.
                 - DistanceToOceanic: Distance in km to nearest oceanic region.
-                - DistanceToContinental: Distance in km to nearest continental region.
+                - DistanceToContinental: Distance in km to nearest continental
+                                         region.
         """
         regions = OrderedDict()
-        point = Point(lon, lat)
-        for layer, fname in self._regions.items():
-            distance_field = 'DistanceTo%s' % layer
-            default_domain = None
-            if layer == 'Stable':
-                default_domain = 'SCR (generic)'
 
-            region, distance = _get_layer_info(layer, point, fname)
+        gd = GeoDict.createDictFromCenter(lon, lat, DX, DY, XSPAN, YSPAN)
 
-            if region is not None:
-                regions['TectonicRegion'] = region
-                regions[distance_field] = 0.0
-            else:
-                regions[distance_field] = distance
+        tec_grid = read(self._tectonic_grid, samplegeodict=gd)
+        region_dict = get_dist_to_type(lon, lat, tec_grid, TECTONIC_REGIONS)
 
-        # Are we oceanic or continental?
-        region, distance = _get_layer_info('Oceanic', point, self._oceanic)
-        if region is not None:
-            regions['Oceanic'] = True
-            # figure out how to get distance to edge of containing polygon (shapely says 0)
-            regions['DistanceToContinental'] = distance
-            regions['DistanceToOceanic'] = 0.0
+        ocean_grid = read(self._oceanic_grid, samplegeodict=gd)
+        ocean_dict = get_dist_to_type(lon, lat, ocean_grid, OCEANIC_REGIONS)
+
+        if region_dict['DistanceToActive'] == 0:
+            region_dict['TectonicRegion'] = 'Active'
+        elif region_dict['DistanceToStable'] == 0:
+            region_dict['TectonicRegion'] = 'Stable'
+        elif region_dict['DistanceToSubduction'] == 0:
+            region_dict['TectonicRegion'] = 'Subduction'
         else:
-            regions['Oceanic'] = False
-            regions['DistanceToOceanic'] = distance
-            regions['DistanceToContinental'] = 0.0
+            region_dict['TectonicRegion'] = 'Volcanic'
 
-        regions = pd.Series(regions, index=['TectonicRegion', 'TectonicDomain', 'DistanceToStable',
-                                            'DistanceToActive', 'DistanceToSubduction',
-                                            'DistanceToVolcanic', 'Oceanic',
-                                            'DistanceToOceanic', 'DistanceToContinental'])
+        region_dict['DistanceToOceanic'] = ocean_dict['DistanceToOceanic']
+        region_dict['DistanceToContinental'] = ocean_dict['DistanceToContinental']
+        region_dict['Oceanic'] = False
+        if ocean_dict['DistanceToOceanic'] == 0:
+            region_dict['Oceanic'] = True
+
+        regions = pd.Series(region_dict, index=['TectonicRegion',
+                                                'DistanceToStable',
+                                                'DistanceToActive',
+                                                'DistanceToSubduction',
+                                                'DistanceToVolcanic',
+                                                'Oceanic',
+                                                'DistanceToOceanic',
+                                                'DistanceToContinental'])
 
         return regions
